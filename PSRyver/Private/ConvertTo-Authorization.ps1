8fa2f12413d5d8386960895e95030effee6a4ec1
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-07-21 10:42:03 -0500

    Add ConvertTo-Authorization.ps1

---
+++ PSRyver/Private/ConvertTo-Authorization.ps1
@@ -0,0 +1,63 @@
+function ConvertTo-Authorization {
+    <#
+    .SYNOPSIS
+        Convert credential objects to basic authorization headers.
+
+    .DESCRIPTION
+        Convert credential objects to basic authorization headers.
+
+    .INPUTS
+        System.Management.Automation.PSCredential
+
+    .NOTES
+        - Troy Lindsay
+        - Twitter: @troylindsay42
+        - GitHub: tlindsay42
+
+    .EXAMPLE
+        ConvertTo-Authorization -Credential ( Get-Credential )
+        Converts the credential to a base64 encoded basic authentication authorization
+        header value.
+
+    .LINK
+        https://tlindsay42.github.io/PSRyver/Private/ConvertTo-Authorization/
+
+    .LINK
+        https://github.com/PSRyver/blob/master/PSRyver/Private/ConvertTo-Authorization.ps1
+    #>
+    param (
+        <#
+        Specifies the credential to convert to a base64 encoded basic authentication
+        authorization header value.
+        #>
+        [Parameter(
+            Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true
+        )]
+        [ValidateNotNull()]
+        [PSCredential]
+        $Credential
+    )
+
+    begin {
+        $function = $MyInvocation.MyCommand.Name
+
+        Write-Verbose -Message (
+            "Beginning: '${function}' with ParameterSetName '$( $PSCmdlet.ParameterSetName )' and Parameters: " +
+            ( $PSBoundParameters | Remove-SensitiveData | Format-Table -AutoSize | Out-String )
+        )
+    }
+
+    process {
+        'Basic ' + [System.Convert]::ToBase64String(
+            [System.Text.Encoding]::ASCII.GetBytes(
+                "$( $Credential.UserName ):$( $Credential.GetNetworkCredential().Password )"
+            )
+        )
+    }
+
+    end {
+        Write-Verbose -Message "Ending: '${function}'."
+    }
+}
