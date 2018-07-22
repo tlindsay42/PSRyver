34f99c9269a634fb2b7dcb183ad74560b44b06ef
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-07-22 14:39:42 -0500

    Add Get-PSRyverConfigPath

---
+++ PSRyver/Public/Get-PSRyverConfigPath.ps1
@@ -0,0 +1,65 @@
+function Get-PSRyverConfigPath {
+    <#
+    .SYNOPSIS
+        Retrieves the PSRyver config file path.
+
+    .DESCRIPTION
+        Retrieves the PSRyver config file path.
+
+    .INPUTS
+        None
+            You cannot pipe input to this cmdlet.
+
+    .NOTES
+        - Troy Lindsay
+        - Twitter: @troylindsay42
+        - GitHub: tlindsay42
+
+    .EXAMPLE
+        Get-PSRyverConfigPath
+        Retrieves the PSRyver config file path.
+
+    .LINK
+        https://tlindsay42.github.io/PSRyver/Public/Get-PSRyverConfigPath/
+
+    .LINK
+        https://github.com/PSRyver/blob/master/PSRyver/Public/Get-PSRyverConfigPath.ps1
+
+    .LINK
+        Set-PSRyverConfigPath
+
+    .FUNCTIONALITY
+        Ryver
+    #>
+    [CmdletBinding(
+        HelpUri = 'https://tlindsay42.github.io/PSRyver/Public/Get-PSRyverConfigPath/'
+    )]
+    [OutputType( [System.IO.FileInfo] )]
+    param ()
+
+    begin {
+        $function = $MyInvocation.MyCommand.Name
+
+        Write-Verbose -Message (
+            "Beginning: '${function}' with ParameterSetName '$( $PSCmdlet.ParameterSetName )' and Parameters: " +
+            ( $PSBoundParameters | Remove-SensitiveData | Format-Table -AutoSize | Out-String )
+        )
+    }
+
+    process {
+        if ( -not $Script:PSRyverConfigFilePath ) {
+            [System.IO.FileInfo] $Script:PSRyverConfigFilePath = Resolve-Path -Path '~' -ErrorAction 'Stop' |
+                Join-Path -ChildPath '.psryver.xml'
+
+            if ( ( Test-Path -Path $Script:PSRyverConfigFilePath ) -eq $true ) {
+                [System.IO.FileInfo] $Script:PSRyverConfigFilePath = Get-Item -Path $Script:PSRyverConfigFilePath -ErrorAction 'Stop'
+            }
+        }
+
+        $Script:PSRyverConfigFilePath
+    }
+
+    end {
+        Write-Verbose -Message "Ending: '${function}'."
+    }
+}
---
+++ docs/Public/Get-PSRyverConfigPath.md
@@ -0,0 +1,51 @@
+# Get-PSRyverConfigPath
+
+## SYNOPSIS
+Retrieves the PSRyver config file path.
+
+## SYNTAX
+
+```
+Get-PSRyverConfigPath [<CommonParameters>]
+```
+
+## DESCRIPTION
+Retrieves the PSRyver config file path.
+
+## EXAMPLES
+
+### EXAMPLE 1
+```
+Get-PSRyverConfigPath
+```
+
+Retrieves the PSRyver config file path.
+
+## PARAMETERS
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable.
+For more information, see about_CommonParameters (http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+### None
+    You cannot pipe input to this cmdlet.
+
+## OUTPUTS
+
+### System.IO.FileInfo
+
+## NOTES
+- Troy Lindsay
+- Twitter: @troylindsay42
+- GitHub: tlindsay42
+
+## RELATED LINKS
+
+[https://tlindsay42.github.io/PSRyver/Public/Get-PSRyverConfigPath/](https://tlindsay42.github.io/PSRyver/Public/Get-PSRyverConfigPath/)
+
+[https://github.com/PSRyver/blob/master/PSRyver/Public/Get-PSRyverConfigPath.ps1](https://github.com/PSRyver/blob/master/PSRyver/Public/Get-PSRyverConfigPath.ps1)
+
+[Set-PSRyverConfigPath]()
+
