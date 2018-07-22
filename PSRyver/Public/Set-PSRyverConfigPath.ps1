8fa6306de8c1275b477fb827f429acba72852d45
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-07-22 17:54:36 -0500

    Add Set-PSRyverConfigPath

---
+++ PSRyver/Public/Set-PSRyverConfigPath.ps1
@@ -0,0 +1,87 @@
+function Set-PSRyverConfigPath {
+    <#
+    .SYNOPSIS
+        Sets the PSRyver config file path.
+
+    .DESCRIPTION
+        Sets the PSRyver config file path.
+
+    .INPUTS
+        System.IO.FileInfo
+
+    .NOTES
+        - Troy Lindsay
+        - Twitter: @troylindsay42
+        - GitHub: tlindsay42
+
+    .EXAMPLE
+        Set-PSRyverConfigPath -Path '~/.psryver.xml'
+        Sets the PSRyver config file path.
+
+    .EXAMPLE
+        '~/.psryver.xml' | Set-PSRyverConfigPath
+        Sets the PSRyver config file path via the pipeline.
+
+    .EXAMPLE
+        Set-PSRyverConfigPath '~/.psryver.xml'
+        Sets the PSRyver config file path via positional parameter.
+
+    .LINK
+        https://tlindsay42.github.io/PSRyver/Public/Set-PSRyverConfigPath/
+
+    .LINK
+        https://github.com/PSRyver/blob/master/PSRyver/Public/Set-PSRyverConfigPath.ps1
+
+    .LINK
+        Get-PSRyverConfigPath
+
+    .FUNCTIONALITY
+        Ryver
+    #>
+    [CmdletBinding(
+        HelpUri = 'https://tlindsay42.github.io/PSRyver/Public/Set-PSRyverConfigPath/',
+        SupportsShouldProcess = $true,
+        ConfirmImpact = 'High'
+    )]
+    [OutputType( [System.IO.FileInfo] )]
+    param (
+        # Specifies the PSRyver config file path that will be used as the default.
+        [Parameter(
+            Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true
+        )]
+        [ValidateScript( { Test-Path -Path $_ -IsValid } )]
+        [System.IO.FileInfo]
+        $Path
+    )
+
+    begin {
+        $function = $MyInvocation.MyCommand.Name
+
+        Write-Verbose -Message (
+            "Beginning: '${function}' with ParameterSetName '$( $PSCmdlet.ParameterSetName )' and Parameters: " +
+            ( $PSBoundParameters | Remove-SensitiveData | Format-Table -AutoSize | Out-String )
+        )
+    }
+
+    process {
+        $proceed = $true
+        if ( $Script:PSRyverConfigFilePath -and $Script:PSRyverConfigFilePath.FullName -ne $Path.FullName ) {
+            $proceed = $PSCmdlet.ShouldProcess( '$Script:PSRyverConfigFilePath', 'Set the default PSRyver config file path for this session' )
+        }
+
+        if ( $proceed ) {
+            if ( ( Test-Path -Path $Path ) -eq $true ) {
+                $Script:PSRyverConfigFilePath = Get-Item -Path $Path -ErrorAction 'Stop'
+            }
+            else {
+                $Script:PSRyverConfigFilePath = $Path
+            }
+        }
+    }
+
+    end {
+        Write-Verbose -Message "Ending: '${function}'."
+    }
+}
---
+++ docs/Public/Set-PSRyverConfigPath.md
@@ -0,0 +1,110 @@
+# Set-PSRyverConfigPath
+
+## SYNOPSIS
+Sets the PSRyver config file path.
+
+## SYNTAX
+
+```
+Set-PSRyverConfigPath [-Path] <FileInfo> [-WhatIf] [-Confirm] [<CommonParameters>]
+```
+
+## DESCRIPTION
+Sets the PSRyver config file path.
+
+## EXAMPLES
+
+### EXAMPLE 1
+```
+Set-PSRyverConfigPath -Path '~/.psryver.xml'
+```
+
+Sets the PSRyver config file path.
+
+### EXAMPLE 2
+```
+'~/.psryver.xml' | Set-PSRyverConfigPath
+```
+
+Sets the PSRyver config file path via the pipeline.
+
+### EXAMPLE 3
+```
+Set-PSRyverConfigPath '~/.psryver.xml'
+```
+
+Sets the PSRyver config file path via positional parameter.
+
+## PARAMETERS
+
+### -Path
+Specifies the PSRyver config file path that will be used as the default.
+
+```yaml
+Type: FileInfo
+Parameter Sets: (All)
+Aliases:
+
+Required: True
+Position: 1
+Default value: None
+Accept pipeline input: True (ByValue)
+Accept wildcard characters: False
+```
+
+### -Confirm
+Prompts you for confirmation before running the cmdlet.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases: cf
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -WhatIf
+Shows what would happen if the cmdlet runs.
+The cmdlet is not run.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases: wi
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable.
+For more information, see about_CommonParameters (http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+### System.IO.FileInfo
+
+## OUTPUTS
+
+### System.IO.FileInfo
+
+## NOTES
+- Troy Lindsay
+- Twitter: @troylindsay42
+- GitHub: tlindsay42
+
+## RELATED LINKS
+
+[https://tlindsay42.github.io/PSRyver/Public/Set-PSRyverConfigPath/](https://tlindsay42.github.io/PSRyver/Public/Set-PSRyverConfigPath/)
+
+[https://github.com/PSRyver/blob/master/PSRyver/Public/Set-PSRyverConfigPath.ps1](https://github.com/PSRyver/blob/master/PSRyver/Public/Set-PSRyverConfigPath.ps1)
+
+[Get-PSRyverConfigPath]()
+
