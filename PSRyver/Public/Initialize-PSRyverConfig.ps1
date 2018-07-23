cddfa6d88a0d30d7144f0f48bb065279b4339757
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-07-23 00:59:22 -0500

    Add Initialize-PSRyverConfig

---
+++ PSRyver/Public/Initialize-PSRyverConfig.ps1
@@ -0,0 +1,184 @@
+function Initialize-PSRyverConfig {
+    <#
+    .SYNOPSIS
+        Initialize the PSRyver module configuration parameters.
+
+    .DESCRIPTION
+        Initialize the PSRyver module configuration parameters stored in the
+        $Script:PSRyver variable.
+
+    .INPUTS
+        None
+            You cannot pipe input to this cmdlet.
+
+    .NOTES
+        - Troy Lindsay
+        - Twitter: @troylindsay42
+        - GitHub: tlindsay42
+
+    .EXAMPLE
+        Initialize-PSRyverConfig
+        Initializes the $Script:PSRyver variable with all module configuration settings
+        set to their default values.
+
+    .EXAMPLE
+        Initialize-PSRyverConfig https://example.ryver.com/api/1/odata.svc ( Get-Credential )
+        Uses positional parameters to set the $Script:PSRyver.RestApiBaseUri value
+        storing the default REST API URI and the $Script:PSRyver.Authorization value
+        storing the basic authentication authorization header to use for all requests.
+
+        The values of all other settings initialized with their default values.
+
+    .EXAMPLE
+        Initialize-PSRyverConfig -IncomingWebhookUri https://example.ryver.com/application/webhook/4z-_G76nj7Fic-M
+        Sets the $Script:PSRyver.IncomingWebhookUri value storing the default
+        incoming webhook URI to use for all requests.
+
+        The values of all other settings initialized with their default values.
+
+    .EXAMPLE
+        [PSCustomObject] @{ Proxy = 'https://192.168.1.1/'; MapUser = $true; ForceVerbose = $true } | Initialize-PSRyverConfig
+        Configures the default proxy, enables user mapping, and prevents sensitive
+        values from being redacted.
+
+        The values of all other settings initialized with their default values.
+
+    .LINK
+        https://tlindsay42.github.io/PSRyver/Public/Initialize-PSRyverConfig/
+
+    .LINK
+        https://github.com/PSRyver/blob/master/PSRyver/Public/Initialize-PSRyverConfig.ps1
+
+    .LINK
+        Get-PSRyverConfig
+
+    .LINK
+        Set-PSRyverConfig
+
+    .LINK
+        Remove-PSRyverConfig
+
+    .LINK
+        Export-PSRyverConfig
+
+    .LINK
+        Import-PSRyverConfig
+
+    .FUNCTIONALITY
+        Ryver
+    #>
+    [CmdletBinding(
+        HelpUri = 'https://tlindsay42.github.io/PSRyver/Public/Initialize-PSRyverConfig/',
+        SupportsShouldProcess = $true,
+        ConfirmImpact = 'High'
+    )]
+    [OutputType( [Void] )]
+    param (
+        # Specifies the REST API URI.  Only serialized to disk on Windows via DPAPI.
+        [Parameter(
+            Position = 0,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [AllowEmptyString()]
+        [ValidatePattern( '^(?:|https://\w+.ryver.com(?::443)?/api/1/odata.svc)$' )]
+        [String]
+        $RestApiBaseUri,
+
+        <#
+        Specify the Credentials to use for basic authentication.
+
+        Only serialized to disk as the base64 encoded authorization header value on
+        Windows via DPAPI.
+        #>
+        [Parameter(
+            Position = 1,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [AllowNull()]
+        [PSCredential]
+        $Credential,
+
+        # Specifies the incoming webhook URI.  Only serialized to disk on Windows via DPAPI.
+        [Parameter(
+            Position = 2,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [AllowEmptyString()]
+        [ValidatePattern( '^(?:|https://\w+.ryver.com(?::443)?/application/webhook/\S+)$' )]
+        [String]
+        $IncomingWebhookUri,
+
+        # Proxy to use with Invoke-RestMethod
+        [Parameter(
+            Position = 3,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [AllowEmptyString()]
+        [ValidatePattern( '^(?:$|https?://\S+)' )]
+        [String]
+        $Proxy,
+
+        <#
+        Specifies whether to generate a map of Ryver user ID to name on module load,
+        for use in Ryver File commands.
+        #>
+        [Parameter(
+            Position = 4,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [Switch]
+        $MapUser = $false,
+
+        <#
+        Prevents sensitive property values from being redacted for troubleshooting
+        purposes.
+
+        *** WARNING ***
+        This will expose your sensitive property values.
+        #>
+        [Parameter(
+            Position = 5,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [Switch]
+        $ForceVerbose = $false
+    )
+
+    begin {
+        $function = $MyInvocation.MyCommand.Name
+
+        Write-Verbose -Message (
+            "Beginning: '${function}' with ParameterSetName '$( $PSCmdlet.ParameterSetName )' and Parameters: " +
+            ( $PSBoundParameters | Remove-SensitiveData | Format-Table -AutoSize | Out-String )
+        )
+    }
+
+    process {
+        $proceed = $true
+        if ( $Script:PSRyver ) {
+            $proceed = $PSCmdlet.ShouldProcess( '$Script:PSRyver', 'Overwrite the existing PSRyver config' )
+        }
+
+        if ( $proceed ) {
+            $Script:PSRyver = [PSCustomObject] @{
+                PSTypeName         = 'PSRyver.Config'
+                RestApiBaseUri     = [Uri] $RestApiBaseUri
+                Authorization      = [String] $null
+                IncomingWebhookUri = [Uri] $IncomingWebhookUri
+                Proxy              = [Uri] $Proxy
+                MapUser            = [Boolean] $MapUser
+                ForceVerbose       = [Boolean] $ForceVerbose
+            }
+
+            if ( $PSBoundParameters.ContainsKey( 'Credential' ) ) {
+                [String] $Script:PSRyver.Authorization = ConvertTo-Authorization -Credential $Credential -ErrorAction 'Stop'
+                $Credential = $null
+                Remove-Variable -Name 'Credential' -Force
+            }
+        }
+    }
+
+    end {
+        Write-Verbose -Message "Ending: '${function}'."
+    }
+}
---
+++ docs/Public/Initialize-PSRyverConfig.md
@@ -0,0 +1,225 @@
+# Initialize-PSRyverConfig
+
+## SYNOPSIS
+Initialize the PSRyver module configuration parameters.
+
+## SYNTAX
+
+```
+Initialize-PSRyverConfig [[-RestApiBaseUri] <String>] [[-Credential] <PSCredential>]
+ [[-IncomingWebhookUri] <String>] [[-Proxy] <String>] [-MapUser] [-ForceVerbose] [-WhatIf] [-Confirm]
+ [<CommonParameters>]
+```
+
+## DESCRIPTION
+Initialize the PSRyver module configuration parameters stored in the
+$Script:PSRyver variable.
+
+## EXAMPLES
+
+### EXAMPLE 1
+```
+Initialize-PSRyverConfig
+```
+
+Initializes the $Script:PSRyver variable with all module configuration settings
+set to their default values.
+
+### EXAMPLE 2
+```
+Initialize-PSRyverConfig https://example.ryver.com/api/1/odata.svc ( Get-Credential )
+```
+
+Uses positional parameters to set the $Script:PSRyver.RestApiBaseUri value
+storing the default REST API URI and the $Script:PSRyver.Authorization value
+storing the basic authentication authorization header to use for all requests.
+
+The values of all other settings initialized with their default values.
+
+### EXAMPLE 3
+```
+Initialize-PSRyverConfig -IncomingWebhookUri https://example.ryver.com/application/webhook/4z-_G76nj7Fic-M
+```
+
+Sets the $Script:PSRyver.IncomingWebhookUri value storing the default
+incoming webhook URI to use for all requests.
+
+The values of all other settings initialized with their default values.
+
+### EXAMPLE 4
+```
+[PSCustomObject] @{ Proxy = 'https://192.168.1.1/'; MapUser = $true; ForceVerbose = $true } | Initialize-PSRyverConfig
+```
+
+Configures the default proxy, enables user mapping, and prevents sensitive
+values from being redacted.
+
+The values of all other settings initialized with their default values.
+
+## PARAMETERS
+
+### -Credential
+Specify the Credentials to use for basic authentication.
+
+Only serialized to disk as the base64 encoded authorization header value on
+Windows via DPAPI.
+
+```yaml
+Type: PSCredential
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 2
+Default value: None
+Accept pipeline input: True (ByPropertyName)
+Accept wildcard characters: False
+```
+
+### -ForceVerbose
+Prevents sensitive property values from being redacted for troubleshooting
+purposes.
+
+*** WARNING ***
+This will expose your sensitive property values.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 6
+Default value: False
+Accept pipeline input: True (ByPropertyName)
+Accept wildcard characters: False
+```
+
+### -IncomingWebhookUri
+Specifies the incoming webhook URI. 
+Only serialized to disk on Windows via DPAPI.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 3
+Default value: None
+Accept pipeline input: True (ByPropertyName)
+Accept wildcard characters: False
+```
+
+### -MapUser
+Specifies whether to generate a map of Ryver user ID to name on module load,
+for use in Ryver File commands.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 5
+Default value: False
+Accept pipeline input: True (ByPropertyName)
+Accept wildcard characters: False
+```
+
+### -Proxy
+Proxy to use with Invoke-RestMethod
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 4
+Default value: None
+Accept pipeline input: True (ByPropertyName)
+Accept wildcard characters: False
+```
+
+### -RestApiBaseUri
+Specifies the REST API URI. 
+Only serialized to disk on Windows via DPAPI.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 1
+Default value: None
+Accept pipeline input: True (ByPropertyName)
+Accept wildcard characters: False
+```
+
+### -Confirm
+Prompts you for confirmation before running the cmdlet.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases: cf
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -WhatIf
+Shows what would happen if the cmdlet runs.
+The cmdlet is not run.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases: wi
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable.
+For more information, see about_CommonParameters (http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+### None
+    You cannot pipe input to this cmdlet.
+
+## OUTPUTS
+
+### System.Void
+
+## NOTES
+- Troy Lindsay
+- Twitter: @troylindsay42
+- GitHub: tlindsay42
+
+## RELATED LINKS
+
+[https://tlindsay42.github.io/PSRyver/Public/Initialize-PSRyverConfig/](https://tlindsay42.github.io/PSRyver/Public/Initialize-PSRyverConfig/)
+
+[https://github.com/PSRyver/blob/master/PSRyver/Public/Initialize-PSRyverConfig.ps1](https://github.com/PSRyver/blob/master/PSRyver/Public/Initialize-PSRyverConfig.ps1)
+
+[Get-PSRyverConfig]()
+
+[Set-PSRyverConfig]()
+
+[Remove-PSRyverConfig]()
+
+[Export-PSRyverConfig]()
+
+[Import-PSRyverConfig]()
+
