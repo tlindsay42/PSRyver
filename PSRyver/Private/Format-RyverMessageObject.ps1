8f7de42d1e4bb89e615c95813585e21914ae7202
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-07-25 07:55:49 -0500

    Add Format-RyverMessageObject

---
+++ PSRyver/Private/Format-RyverMessageObject.ps1
@@ -0,0 +1,106 @@
+function Format-RyverMessageObject {
+    <#
+    .SYNOPSIS
+        Parse message objects.
+
+    .DESCRIPTION
+        Parse message objects.
+
+    .INPUTS
+        System.Management.Automation.PSCustomObject[]
+
+    .INPUTS
+        System.Management.Automation.PSCustomObject
+
+    .NOTES
+        - Troy Lindsay
+        - Twitter: @troylindsay42
+        - GitHub: tlindsay42
+
+    .EXAMPLE
+        Format-RyverMessageObject -InputObject $objects
+        Parses the message objects.
+
+    .EXAMPLE
+        $objects | Format-RyverMessageObject
+        Parses the message objects via the pipeline.
+
+    .EXAMPLE
+        Format-RyverMessageObject $objects
+        Parses the message objects via positional parameter.
+
+    .LINK
+        https://tlindsay42.github.io/PSRyver/Private/Format-RyverMessageObject/
+
+    .LINK
+        https://github.com/PSRyver/blob/master/PSRyver/Private/Format-RyverMessageObject.ps1
+
+    .FUNCTIONALITY
+        Ryver
+    #>
+    [CmdletBinding(
+        HelpUri = 'https://tlindsay42.github.io/PSRyver/Private/Format-RyverMessageObject/'
+    )]
+    [OutputType( [PSCustomObject[]] )]
+    [OutputType( [PSCustomObject] )]
+    param (
+        # The public forum or private team channel objects to parse.
+        [Parameter(
+            Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true
+        )]
+        [PSCustomObject[]]
+        $InputObject
+    )
+
+    begin {
+        $function = $MyInvocation.MyCommand.Name
+
+        Write-Verbose -Message (
+            "Beginning: '${function}' with ParameterSetName '$( $PSCmdlet.ParameterSetName )' and Parameters: " +
+            ( $PSBoundParameters | Remove-SensitiveData | Format-Table -AutoSize | Out-String )
+        )
+    }
+
+    process {
+        foreach ( $object in $InputObject ) {
+            [PSCustomObject] @{
+                PSTypeName   = 'PSRyver.Entity.Message'
+                ID           = $object.ID
+                MessageType  = $object.MessageType
+                CreateDate   = $object.When
+                ModifyDate   = $object.ModifyDate
+                CreateSource = [PSCustomObject] @{
+                    DisplayName = $object.CreateSource.DisplayName
+                    Avatar      = $object.CreateSource.Avatar
+                }
+                ModifySource = $object.ModifySource
+                To = [PSCustomObject] @{
+                    PSTypeName = "PSRyver.$( $object.From.__Metadata.Type )"
+                    Metadata   = [PSCustomObject] @{
+                        Type = $object.To.__Metadata.Type
+                    }
+                    ID         = $object.To.ID
+                    Descriptor = $object.To.__Descriptor
+                    JID        = $object.To.JID
+                }
+                From = [PSCustomObject] @{
+                    PSTypeName = "PSRyver.$( $object.From.__Metadata.Type )"
+                    Metadata   = [PSCustomObject] @{
+                        Type = $object.From.__Metadata.Type
+                    }
+                    ID         = $object.From.ID
+                    Descriptor = $object.From.__Descriptor
+                    JID        = $object.From.JID
+                }
+                Reactions      = $object.__Reactions
+                Embeds         = $object.Embeds
+            }
+        }
+    }
+
+    end {
+        Write-Verbose -Message "Ending: '${function}'."
+    }
+}
---
+++ docs/Private/Format-RyverMessageObject.md
@@ -0,0 +1,81 @@
+# Format-RyverMessageObject
+
+## SYNOPSIS
+Parse message objects.
+
+## SYNTAX
+
+```
+Format-RyverMessageObject [-InputObject] <PSObject[]> [<CommonParameters>]
+```
+
+## DESCRIPTION
+Parse message objects.
+
+## EXAMPLES
+
+### EXAMPLE 1
+```
+Format-RyverMessageObject -InputObject $objects
+```
+
+Parses the message objects.
+
+### EXAMPLE 2
+```
+$objects | Format-RyverMessageObject
+```
+
+Parses the message objects via the pipeline.
+
+### EXAMPLE 3
+```
+Format-RyverMessageObject $objects
+```
+
+Parses the message objects via positional parameter.
+
+## PARAMETERS
+
+### -InputObject
+The public forum or private team channel objects to parse.
+
+```yaml
+Type: PSObject[]
+Parameter Sets: (All)
+Aliases:
+
+Required: True
+Position: 1
+Default value: None
+Accept pipeline input: True (ByValue)
+Accept wildcard characters: False
+```
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable.
+For more information, see about_CommonParameters (http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+### System.Management.Automation.PSCustomObject[]
+
+### System.Management.Automation.PSCustomObject
+
+## OUTPUTS
+
+### System.Management.Automation.PSObject[]
+
+### System.Management.Automation.PSObject
+
+## NOTES
+- Troy Lindsay
+- Twitter: @troylindsay42
+- GitHub: tlindsay42
+
+## RELATED LINKS
+
+[https://tlindsay42.github.io/PSRyver/Private/Format-RyverMessageObject/](https://tlindsay42.github.io/PSRyver/Private/Format-RyverMessageObject/)
+
+[https://github.com/PSRyver/blob/master/PSRyver/Private/Format-RyverMessageObject.ps1](https://github.com/PSRyver/blob/master/PSRyver/Private/Format-RyverMessageObject.ps1)
+
