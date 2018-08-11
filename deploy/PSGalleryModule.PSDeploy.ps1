f5933709a2179942355ad6529e71d946fd1224d9
Troy Lindsay <tlindsay42@users.noreply.github.com>
2018-08-10 22:05:55 -0500

    Replace PSDeploy script

---
+++ deploy/PSGalleryModule.PSDeploy.ps1
@@ -0,0 +1,9 @@
+﻿Deploy 'Module' {
+    By 'PSGalleryModule' {
+        FromSource $Global:CI_MODULE_NAME
+        To 'PSGallery'
+        WithOptions @{
+            ApiKey = $Env:NUGET_API_KEY
+        }
+    }
+}
